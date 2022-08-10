74bcb2d7e7525169aab699b0a91a341eb78466fe
Yauheni Azaronak <jonarizzzz@gmail.com>
2022-08-10 14:59:27 +0400

    Spring + RomanToInteger

---
+++ src/main/java/com/example/demo/begginer/RomanToInteger.java
@@ -0,0 +1,62 @@
+package com.example.demo.begginer;
+
+import java.util.Objects;
+
+public class RomanToInteger {
+
+//    Runtime: 8 ms, faster than 64.08% of Java online submissions for Roman to Integer.
+//    Memory Usage: 45.1 MB, less than 69.78% of Java online submissions for Roman to Integer.
+
+//    Symbol       Value
+//    I             1
+//    V             5
+//    X             10
+//    L             50
+//    C             100
+//    D             500
+//    M             1000
+
+
+//    For example, 2 is written as II in Roman numeral, just two ones added together. 12 is written as XII,
+//    which is simply X + II. The number 27 is written as XXVII, which is XX + V + II.
+//
+//    Roman numerals are usually written largest to smallest from left to right.
+//    However, the numeral for four is not IIII. Instead, the number four is written as IV.
+//    Because the one is before the five we subtract it making four. The same principle applies
+//    to the number nine, which is written as IX. There are six instances where subtraction is used:
+//
+//    I can be placed before V (5) and X (10) to make 4 and 9.
+//    X can be placed before L (50) and C (100) to make 40 and 90.
+//    C can be placed before D (500) and M (1000) to make 400 and 900.
+//    Given a roman numeral, convert it to an integer.
+
+    public static int romanToInt(String s) {
+        int result = 0;
+
+        for (int i = 0; i < s.length(); i++){
+            if (i + 1 == s.length()){
+                result += translateSymbol(s.charAt(i));
+            } else if (translateSymbol(s.charAt(i)) > translateSymbol(s.charAt(i + 1)) ||
+                    Objects.equals(translateSymbol(s.charAt(i)), translateSymbol(s.charAt(i + 1)))){
+                result += translateSymbol(s.charAt(i));
+            } else {
+                result -= translateSymbol(s.charAt(i));
+            }
+        }
+        return result;
+    }
+
+    private static int translateSymbol(char symbol){
+        return switch (symbol) {
+            case 'I' -> 1;
+            case 'V' -> 5;
+            case 'X' -> 10;
+            case 'L' -> 50;
+            case 'C' -> 100;
+            case 'D' -> 500;
+            case 'M' -> 1000;
+            default -> throw new IllegalArgumentException("Not roman character used!");
+        };
+    }
+
+}
---
+++ src/test/java/com/example/demo/begginer/RomanToIntegerTest.java
@@ -0,0 +1,29 @@
+package com.example.demo.begginer;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class RomanToIntegerTest {
+
+    @Test
+    @DisplayName("III")
+    void romanToInt1() {
+        Assertions.assertEquals(3, RomanToInteger.romanToInt("III"));
+    }
+
+    @Test
+    @DisplayName("MCMXCIV")
+    void romanToInt2() {
+        Assertions.assertEquals(1994, RomanToInteger.romanToInt("MCMXCIV"));
+    }
+
+    @Test
+    @DisplayName("MMCCCXCIX")
+    void romanToInt3() {
+        Assertions.assertEquals(2399, RomanToInteger.romanToInt("MMCCCXCIX"));
+    }
+
+}
